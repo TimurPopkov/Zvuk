53e3ce3b9a7e593ff211100ec5785aee70198d9a
timurpopkov <popkov@self.team>
2023-09-28 16:19:58 +0400

    1

--- Test Suites/Smoke/Smoke.ts
+++ Test Suites/Smoke/Smoke.ts
@@ -21,14 +21,14 @@
    <testCaseLink>
       <guid>91f88bec-1768-4f85-a8aa-b5d59981ecb0</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Регистрация. По почте/Регистрация. По почте. Успешное флоу</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>db66ac0b-f82f-4cc4-8b12-8c57882c07ea</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Подтверждение почты. После регистрации по почте/Подтверждение почты. После регистрации по почте. Страница Подтверждения почты</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
@@ -47,28 +47,28 @@
    <testCaseLink>
       <guid>5f8b8eb8-4f92-4048-b8ab-22594ee189fb</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Подтверждение почты. После регистрации по почте/Подтверждение почты. Переход по ссылке</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>52e2c76c-2c59-4763-a2c6-e9066a904d65</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Авторизация/Выход из приложения</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>bb0ffabb-9a4e-46c5-9932-74bdd7cd2d8e</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Авторизация/Авторизация. Восстановление пароля/Авторизация. Восстановление пароля. Успешное флоу</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>8bddd065-5e65-4ff5-af33-add9dbd0b212</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Авторизация/Авторизация. Успешное флоу</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
@@ -81,35 +81,35 @@
    <testCaseLink>
       <guid>0f73783e-c5b0-4372-9ac6-0324c8df6dc0</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Управление подкастами/Создание подкаста/Создание подкаста. Успешный флоу</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>270ba0f3-aa59-4f42-ab95-7ef561c8547a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Управление эпизодами/Добавление эпизода/Добавление эпизода. Опубликовать позже</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>316ef8d5-1fda-4aeb-9492-048a37357b8b</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Управление эпизодами/Добавление эпизода/Добавление эпизода. Нет опубликованных эпизодов. Опубликовать эпизод</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>df945dd5-1f42-42e3-a608-b999d1cd3cd2</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Управление эпизодами/Добавление эпизода/Добавление эпизода. Сохранить черновик</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>9d5c7490-1e52-4d5d-baff-d77aa8cfc6d9</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RSS/Проверка RSS в валидаторе</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
       <variableLink>
@@ -122,63 +122,63 @@
    <testCaseLink>
       <guid>428da866-100b-4a25-b303-9bd2ef5e2163</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RSS/Открытие аудиофайла из RSS. Новый подкаст</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>816608d1-2b99-4800-a70f-e6cb35f5bbe5</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RSS/Открытие обложек из RSS. Новый подкаст</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>6b7b4bcd-251b-4c2c-b894-d0a19cdbdf6a</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/RSS/Ссылка на мини-сайт эпизода в RSS</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>b68c6c42-d86c-4cad-bf63-b0d4b5e93848</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Мини-сайт/Мини-сайт. Страница</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>95c1fadc-3bc7-4577-bcc8-783337deb387</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Мини-сайт/Мини-сайт. Прослушивание трека</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>3070fe4f-1694-4b94-a92f-2cded2bb99c4</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Мини-сайт/Мини-сайт. Переключение треков. Следующий трек</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>24a564ad-32d6-4241-93b6-643040953475</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Мини-сайт/Мини-сайт. Переключение треков. Предыдущий трек</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>a90e32f3-f5ee-49ef-92a2-0ac9883b84c3</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Импорт подкаста/Импорт подкаста. Успешный импорт</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
    <testCaseLink>
       <guid>7a283f74-d7aa-491c-8917-ade1b86f9011</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/APM/Восстановление пароля/АРМ. Успешное восстановление пароля</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
@@ -199,7 +199,7 @@
    <testCaseLink>
       <guid>60fcb119-a33b-44c2-9280-9f39097515a3</guid>
       <isReuseDriver>false</isReuseDriver>
-      <isRun>false</isRun>
+      <isRun>true</isRun>
       <testCaseId>Test Cases/Главная/Главная. Статистика. Состояние статистики/Виджеты/Главная. Статистика. Новый уникальный слушатель. Сегодня</testCaseId>
       <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
    </testCaseLink>
